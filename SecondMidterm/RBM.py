4060f468051351f7195b29d012f6d8c8eb0595e0
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2021-04-13 23:42:19 +0200

    Add files via upload

--- SecondMidterm/RBM.py
+++ SecondMidterm/RBM.py
@@ -5,7 +5,7 @@ import csv
 class RBM:
 
     def __init__(self, n_visible, n_hidden):
-        self.weights = np.random.normal(0, 0.01, (n_hidden, n_visible))
+        self.weights = np.random.normal(0, 0.1, (n_hidden, n_visible))
         self.visible_bias = np.zeros(n_visible)
         self.hidden_bias = np.zeros(n_hidden)
 
@@ -25,9 +25,6 @@ class RBM:
                     w_writer.writerow(w)
 
                 w_writer.writerows([["# Visible Bias"], self.visible_bias, ["# Hidden Bias"], self.hidden_bias])
-                # w_writer.writerow(self.visibile_bias)
-                # w_writer.writerows(["# Hidden Bias"])
-                # w_writer.writerow(self.hidden_bias)
 
         except IOError:
             print("It was impossible to retrieve or write data on file " + file)
@@ -36,58 +33,61 @@ class RBM:
         try:
             with open(file) as datafile:
                 filereader = csv.reader(datafile, delimiter=",")
-                hidden = False
-                bias = False
                 for row, i in zip(filereader, range(len(self.weights))):
-                    if row[0][0] == "#":
-                        bias = True
-                        # continue
-                    elif not hidden and bias:
-                        self.visible_bias = row
+                    self.weights[i] = row
+
+                hidden = False
+                for row in filereader:
+                    if (filereader.line_num - len(self.weights)) % 2 == 1:
+                        continue
+
+                    if not hidden:
+                        self.visible_bias = np.array(row).astype("float32")
                         hidden = True
-                    elif hidden and bias:
-                        self.hidden_bias = row
                     else:
-                        self.weights[i] = row
+                        self.hidden_bias = np.array(row).astype("float32")
 
         except IOError:
             print("It was impossible to retrieve or read data on file " + file)
 
     def train(self, tr_set, eta, epochs: int, save_weights: bool):
-        # self.__load_weights("weights.csv")
         for i in range(epochs):
+            np.random.shuffle(tr_set)
             bin_tr_set = self.__clamp_data(tr_set)
+            # mean_error = 0
             for pattern, bin_pattern in zip(tr_set, bin_tr_set):
                 # wake
-                # clamped_pattern = self.__clamp_data(pattern)
-                # hidden_nets = np.dot(self.weights, binary_pattern) + self.hidden_bias
                 h_nets = np.dot(self.weights, bin_pattern) + self.hidden_bias
-                # hidden_p = 1/(1+np.exp(-hidden_nets))  # .astype("float32")
                 h_p = self.__sigmoid(h_nets)
-                # wake = np.outer(hidden_p, pattern)
                 wake = np.outer(h_p, pattern)
 
                 # dream
-                # stochastic_hidden_p = self.__clamp_data(hidden_p)
                 bin_h_p = self.__clamp_data(h_p)
-                # visible_nets = np.dot(self.weights.T, stochastic_hidden_p) + self.visibile_bias
                 v_nets = np.dot(self.weights.T, bin_h_p) + self.visible_bias
-                # visible_p = 1/(1+np.exp(-visible_nets))
                 v_p = self.__sigmoid(v_nets)
-                # stochastic_reconstruction = self.__clamp_data(visible_p)
                 bin_reconstruction = self.__clamp_data(v_p)
-                # negative_hidden_p = 1/(1 + np.exp(-np.dot(self.weights, stochastic_reconstruction)-self.hidden_bias))
                 neg_h_nets = np.dot(self.weights, bin_reconstruction) + self.hidden_bias
                 neg_h_p = self.__sigmoid(neg_h_nets)
-                # dream = np.outer(negative_hidden_p, stochastic_reconstruction)
                 dream = np.outer(neg_h_p, bin_reconstruction)
 
                 self.weights += eta * (wake - dream)/len(tr_set)
-                # self.visibile_bias += eta * (np.sum(hidden_p) - np.sum(negative_hidden_p))/len(tr_set)
-                self.visible_bias += eta * (np.sum(h_p) - np.sum(neg_h_p)) / len(tr_set)
-                # self.hidden_bias += eta * (np.sum(binary_pattern) - np.sum(stochastic_reconstruction))/len(tr_set)
-                self.hidden_bias += eta * (np.sum(bin_pattern) - np.sum(bin_reconstruction)) / len(tr_set)
-                # break
-            # break
+                self.visible_bias += eta * (np.sum(pattern) - np.sum(bin_reconstruction)) / len(tr_set)
+                self.hidden_bias += eta * (np.sum(h_p) - np.sum(neg_h_p)) / len(tr_set)
+                # mean_error += np.sum(np.power(bin_pattern - bin_reconstruction, 2))
+
+            # print("Epoch {0}, mean error {1}".format(i, mean_error/len(tr_set)))
+
         if save_weights:
             self.__save_weights("weights.csv")
+
+    def encode(self, patterns, load_weigths: bool):
+        if load_weigths:
+            self.__load_weights("weights.csv")
+
+        y = list()
+        bin_patterns = self.__clamp_data(patterns)
+        for bin_pattern in bin_patterns:
+            h_nets = np.dot(self.weights, bin_pattern) + self.visible_bias
+            y.append(self.__sigmoid(h_nets))
+
+        return np.array(y)
--- SecondMidterm/main.py
+++ SecondMidterm/main.py
@@ -1,14 +1,20 @@
 # import keras
 from keras.datasets import mnist
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 from ISPR.SecondMidterm.RBM import *
+import time
 
 if __name__ == '__main__':
     (tr_set, tr_labels), (ts_set, ts_labels) = mnist.load_data()
+    plt.plot(tr_set[0])
+    # plt.show()
     tr_set = tr_set.astype("float32")/255
     ts_set = ts_set.astype("float32")/255
     tr_set = np.array([np.reshape(pattern, 784) for pattern in tr_set])
     ts_set = np.array([np.reshape(pattern, 784) for pattern in ts_set])
     rbm = RBM(tr_set.shape[1], 100)
-    rbm.train(tr_set, 0.01, 1, True)
+    t = time.time()
+    rbm.train(tr_set, 0.3, 5, True)
+    print(time.time() - t)
+    encodings = rbm.encode(tr_set, False)
